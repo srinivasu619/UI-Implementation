475947be830dd1525f566b5ee83778fe8e879361
srinivasu619 <srinivasu.618@gmail.com>
2017-07-23 00:10:23 +0530

    Added some changes in HTML and minor tweaks in CSS

--- UI NO 13-Calorie Meter/script.js
+++ UI NO 13-Calorie Meter/script.js
@@ -8,7 +8,7 @@
                     svgStyle: null
                     });
 bar.setText('&nbsp;&nbsp;&nbsp;&nbsp;3039 <br> Your Goal');
-bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontFamily = '"Athiti", sans-serif';
 bar.text.style.fontSize = '1rem';
 bar.text.style.color='#000';
 bar.animate(0.68); 
