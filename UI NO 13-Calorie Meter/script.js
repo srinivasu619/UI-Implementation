03e478923c029bb6fe7443d155d99d321ff27dd2
srinivasu619 <srinivasu.618@gmail.com>
2017-07-23 21:00:13 +0530

    Added two other progress bars

--- UI NO 13-Calorie Meter/script.js
+++ UI NO 13-Calorie Meter/script.js
@@ -12,3 +12,23 @@ bar.text.style.fontFamily = '"Athiti", sans-serif';
 bar.text.style.fontSize = '1rem';
 bar.text.style.color='#000';
 bar.animate(0.68); 
+ var bar1 = new ProgressBar.Circle(protien, {
+                    strokeWidth: 5,
+                    easing: 'easeInOut',
+                    duration: 1400,
+                    color: 'red',
+                    trailColor: '#595959',
+                    trailWidth: 4,
+                    svgStyle: null
+                    });
+bar1.animate(0.68); 
+var bar2 = new ProgressBar.Circle(carbs, {
+                    strokeWidth: 5,
+                    easing: 'easeInOut',
+                    duration: 1400,
+                    color: 'red',
+                    trailColor: '#595959',
+                    trailWidth: 4,
+                    svgStyle: null
+                    });
+bar2.animate(0.68); 
