3d2582231a0c0cfe6f84570b9e6f7e071f609b6c
srinivasu619 <srinivasu.618@gmail.com>
2017-09-04 21:14:55 +0530

    Added Scripting for the progress bar and add missing links

---
+++ UI NO 27-Ratings and Reviews UI/script.js
@@ -0,0 +1,57 @@
+var bar = new ProgressBar.Line(one, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#27ae60',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '70%', height: '100%',borderRadius:'1.2em'}
+});
+
+bar.animate(1496/1496); 
+bar = new ProgressBar.Line(two, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#2ecc71',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '70%', height: '100%',borderRadius:'1.2em'}
+});
+
+bar.animate(752/1496); 
+bar = new ProgressBar.Line(three, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#1abc9c',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '70%', height: '100%',borderRadius:'1.2em'}
+});
+
+bar.animate(309/1496);  
+//#f1c40f
+bar = new ProgressBar.Line(four, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#f1c40f',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '70%', height: '100%',borderRadius:'1.2em'}
+});
+
+bar.animate(121/1496);  
+
+bar = new ProgressBar.Line(five, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#e74c3c',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '70%', height: '100%',borderRadius:'1.2em'}
+});
+
+bar.animate(285/1496);  
